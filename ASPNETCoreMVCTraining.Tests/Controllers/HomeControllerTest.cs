1c9cb74dd6fbf082eeb8a55930c27a2d8df44f23
Nguyen Thanh Xuan <thanhxuanhd007@gmail.com>
2024-02-22 09:28:16 +0700

    Add example Unit test

---
+++ ASPNETCoreMVCTraining.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using ASPNETCoreMVCTraining.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ASPNETCoreMVCTraining.Tests.Controllers;
+
+[TestFixture]
+public class HomeControllerTest
+{
+    private Mock<ILogger<HomeController>> _loggerMock;
+    private HomeController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        _loggerMock = new();
+
+        _controller = new HomeController(_loggerMock.Object);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _controller?.Dispose();
+    }
+
+    [Test]
+    public void Index_RequestToPage_ReturnView()
+    {
+        // Arrange
+        // Act
+        var actual = _controller.Index();
+
+        // Assert
+        Assert.That(actual, Is.Not.Null);
+    }
+}
---
+++ ASPNETCoreMVCTraining.Tests/Services/DeptServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using ASPNETCoreMVCTraining.Models;
+using ASPNETCoreMVCTraining.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCoreMVCTraining.Tests.Services;
+
+[TestFixture]
+public class DeptServiceTests
+{
+    private ApplicationDbContext _applicationDbContext;
+    private DeptService _deptService;
+    private List<Dept> _depts;
+
+    [SetUp]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+          .UseInMemoryDatabase(databaseName: "ASPNETCoreMVCTrainingDb")
+          .Options;
+        _applicationDbContext = new ApplicationDbContext(options);
+        _deptService = new DeptService(_applicationDbContext);
+        _depts = [];
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (_depts.Count is not 0)
+        {
+            _applicationDbContext?.RemoveRange(_depts);
+            _applicationDbContext?.SaveChanges();
+        }
+        _applicationDbContext?.Dispose();
+    }
+
+    [TestCase(100)]
+    [TestCase(200)]
+    public void GetById_WhenDeptNotFound_ReturnNewDept(int id)
+    {
+        // Arrange
+        // Act
+        var actual = _deptService.GetById(id);
+
+        // Assert
+        Assert.That(actual, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Id, Is.Null);
+            Assert.That(actual.Name, Is.Null);
+        });
+    }
+
+    [Test]
+    public void GetById_WhenDeptExistInDatabase_ReturnDept()
+    {
+        // Arrange
+        var dept = new Dept { Id = 100, Name = "TestDept" };
+        _applicationDbContext.Depts.Add(dept);
+        _applicationDbContext.SaveChanges();
+
+        _depts.Add(dept);
+
+        // Act
+        var actual = _deptService.GetById(100);
+
+        // Assert
+        Assert.That(actual, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Id, Is.EqualTo(100));
+            Assert.That(actual.Name, Is.EqualTo("TestDept"));
+        });
+    }
+}
